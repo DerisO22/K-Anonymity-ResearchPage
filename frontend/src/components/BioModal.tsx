cea959680f59d83c5b0c14f4007830fc7ae188d7
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-05-25 17:19:36 -0400

    Fixed the home section background bezier img

--- frontend/src/components/BioModal.tsx
+++ frontend/src/components/BioModal.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import type { Profile } from "../util/consts/profiles";
+import { profile_bios } from "../util/consts/profiles";
 
 const BioModal = () => {
     const { name } = useParams();
+    const [ teammateData, setTeammateData ] = useState<Profile | null>();
 
     useEffect(() => {
-        
+        setTeammateData(profile_bios[name.toLowerCase()]);
+
+        console.log(teammateData);
     }, []);
 
     return (
