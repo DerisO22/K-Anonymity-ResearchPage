14659d698ac131753396b53610ba3f2d244b5315
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-05-25 17:31:20 -0400

    added basic html structure for footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,21 @@
 const Footer = () => {
     return (
-        <div>
-        
+        <div className="footer">
+            <div className="footer_logo"></div>
+
+            <div className="footer_links_container">
+                <button className="footer_link"></button>
+                <button className="footer_link"></button>
+                <button className="footer_link"></button>
+            </div>
+
+            <div className="socials_container">
+                <button className="social_button"></button>
+                <button className="social_button"></button>
+                <button className="social_button"></button>
+            </div>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
