1aca40c231e8f2fe4236ba7c4efbc4369ef435e9
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-05-24 22:09:17 -0400

    start to team section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,47 +1,49 @@
 import '../styles/pages.css';
 import '../styles/common_styles.css';
+import '../styles/homepage.css';
 import { useNavigate } from 'react-router-dom';
+import profilePlaceholder from '../assets/profile_placeholder.jpg';
 
 export interface Teammate {
 	name: string,
 	role: string,
 	study: string,
 	img_src: string
-}
+};
 
 const team_info: Teammate[] = [
 	{
 		name: "Vikas Gowda",
-		role: "Programmer",
+		role: "Lead",
 		study: "",
-		img_src: "",
+		img_src: profilePlaceholder,
 	},
 	{
 		name: "Connor Hill",
 		role: "Programmer",
 		study: "Computer Science",
-		img_src: "",
+		img_src: profilePlaceholder,
 	},
 	{
 		name: "Andy Zheng",
 		role: "Programmer",
 		study: "Computer Science",
-		img_src: "",
+		img_src: profilePlaceholder,
 		
 	},
 	{
 		name: "Deris O'Malley",
 		role: "Programmer",
 		study: "Computer Science",
-		img_src: "",
+		img_src: profilePlaceholder,
 	},
 	{
 		name: "Genensis BautistaSanchez",
 		role: "Editor",
 		study: "Computer Science",
-		img_src: "",
+		img_src: profilePlaceholder,
 	},
-]
+];
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -57,11 +59,16 @@ const Home = () => {
 			<h2>Our Team</h2>
 			<div className="team_container">
 				{team_info.map((teammate) => (
-					<div className='teammate_card'>
-						<img src={teammate.img_src} />
+					<div className='teammate_card' key={teammate.name}>
+						<img src={teammate.img_src} className='profile_image'/>
 
-						<button onClick={() => navigate_to_bio(teammate.name)} className='bio_navigate_button'></button>
+						<button onClick={() => navigate_to_bio(teammate.name)} className='bio_navigate_button'>Bio</button>
 
+						<div className="team_card_text_container">
+							<p className='text'>{teammate.name}</p>
+							<p className="text">{teammate.role}</p>
+							<p className="text">{teammate.study}</p>
+						</div>
 					</div>
 				))}
 			</div>
